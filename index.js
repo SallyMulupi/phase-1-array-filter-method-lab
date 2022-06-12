072d9e673b0ade5af7d41a04e95c68206cd7d039
SallyMulupi <mulupisally2gmail.com>
2022-06-12 17:18:51 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+function findMatching(arr, query) {
+    return arr.filter(function(el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    })
+}
+
+
+
+function fuzzyMatch(arr, query) {
+    return arr.filter(function(el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) == 0
+    })
+}
+
+
+function matchName(arr, query) {
+    return arr.filter(function(el) {
+        if (el.name === query) {
+            return el;
+        }
+    })
+
+}
